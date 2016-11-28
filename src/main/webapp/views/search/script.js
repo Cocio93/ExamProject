2224c5f4c1e5cfeac7726795e4511a2a5acc746a
John <John@10.50.130.0>
2016-11-28 13:40:45 +0100

    Some error handling for search function have been added, date is partly fixed. Date is now only missing last date when using flexSearch

--- src/main/webapp/views/search/script.js
+++ src/main/webapp/views/search/script.js
@@ -4,62 +4,71 @@ var app = angular.module('myApp.searchModule', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/search', {
             templateUrl: 'views/search/SearchPage.html',
-            controller: 'searchCtrl',
+            controller: 'searchCtrl'
         });
     }]);
 
 app.controller('searchCtrl', ['TicketFactory', '$scope', function (TicketFactory, $scope) {
-        $scope.airports = [{code: 'CPH', name: 'Copenhagen'}, {code: 'STN', name: 'London'}, {code: 'BCN', name: 'Barcelona'}, {code: 'PAR', name: 'Paris'}, {code: 'SXF', name: 'Berlin'}];
+        $scope.airports = [{code: 'CPH', name: 'Copenhagen'}, {code: 'STN', name: 'London'}, {code: 'BCN', name: 'Barcelona'}, {code: 'CDG', name: 'Paris'}, {code: 'SXF', name: 'Berlin'}];
         $scope.searchParams = {from: '', to: '', date: '', tickets: 1, flexdate: ''};
         $scope.searchResults = [];
         $scope.isSearched = false;
         $scope.flextoggle = false;
-        $scope.isFlexSearched = false;
 
         $scope.getSearchResults = function () {
-            var date = $scope.searchParams.date;
-            if ($scope.flextoggle === true) {
-                var flexdate = $scope.searchParams.flexdate;
-                var arr = [];
-                while (date <= flexdate) {
-                    $scope.searchResults = TicketFactory.getFromTickets(
-                            $scope.searchParams.from,
-                            $scope.searchParams.date,
-                            $scope.searchParams.tickets
-                            ).success((function (data) {
-                        arr.push(data);
-                    }
-                    ));
-                    $scope.searchResults = arr;
-                    date.setDate(date.getDate() + 1);
-                    $scope.isSearched = false;
-                    $scope.isFlexSearched = true;
-                }
+            $scope.searchResults = [];
+            if ($scope.searchParams.date === "" || $scope.searchParams.from === "" || ($scope.flextoggle === true && $scope.searchParams.flexdate === "")) {
+                console.log("Something was not filled out properly, returning");
+                $scope.isSearched = false;
+                return true;
             } else {
-                $scope.searchResults = TicketFactory.getFromTickets(
-                        $scope.searchParams.from,
-                        $scope.searchParams.date,
-                        $scope.searchParams.tickets)
-                        .success(function (data) {
-                            var tmp = [];
-                            tmp.push(data);
-                            $scope.searchResults = tmp;
-                            console.log("results : " + tmp);
-                        });
-                $scope.isFlexSearched = false;
                 $scope.isSearched = true;
             }
+
+            var tickets = $scope.searchParams.tickets;
+            var to = $scope.searchParams.to;
+            var from = $scope.searchParams.from;
+            var date = $scope.searchParams.date;
+            var flexdate = $scope.searchParams.flexdate;
+            date.setDate(date.getDate() + 1);
+
+            if ($scope.flextoggle === true) {
+                var arr = [];
+                while (date <= flexdate) {
+                    $scope.searchResults = TicketFactory.getFromTickets(from, date, tickets)
+                            .success((function (data) {
+                                arr.push(data);
+                            }
+                            ));
+                    $scope.searchResults = arr;
+                    date.setDate(date.getDate() + 1);
+                }
+            } else {
+                $scope.searchResults = TicketFactory.getFromTickets(from, date, tickets)
+                        .success(function (data) {
+                            var arr = [];
+                            arr.push(data);
+                            $scope.searchResults = arr;
+                        });
+            }
+            date.setDate(date.getDate() - 1);
         };
 
         $scope.minutesToHours = function (time) {
             var hours = Math.trunc(time / 60);
             var minutes = time % 60;
-            var res = hours + " hour(s) and " +  minutes + " minutes";
-            return res;
-            
+            var hourString = "Hour";
+            if (hours > 1) {
+                hourString = "Hours";
+            }
+            if (minutes === 0) {
+                return hours + " " + hourString;
+            } else {
+                return hours + " " + hourString + " and " + minutes + " Minutes";
+            }
         };
-        
-        $scope.iATAToName = function(code) {
+
+        $scope.iATAToName = function (code) {
             var name = '';
             for (var i = 0; i < $scope.airports.length; i++) {
                 if ($scope.airports[i].code === code) {
@@ -74,7 +83,6 @@ app.factory('TicketFactory', function ($http) {
     var baseUrl = 'http://airline-plaul.rhcloud.com/api/flightinfo';
     return {
         getFromTickets: function (from, date, tickets) {
-            console.log(date.toISOString() + 1);
             return $http({
                 url: baseUrl + '/' + from + '/' + date.toISOString() + '/' + tickets,
                 method: 'get'
