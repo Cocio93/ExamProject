e8d418c121f9986c1a78acbc8989d551aa12bcab
cph-ds117 <cph-ds117@CPH-R9GP0BR.efif.dk>
2016-11-30 23:38:22 +0100

    Removed a controller that was added in the wrong script.js (Was in signModule)

--- src/main/webapp/views/createUser/script.js
+++ src/main/webapp/views/createUser/script.js
@@ -2,7 +2,6 @@ var app = angular.module('myApp.signupModule', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/signup', {
-            templateUrl: 'views/createUser/CreateUser.html',
-            controller: 'reservationCtrl'
+            templateUrl: 'views/createUser/CreateUser.html'
         });
     }]);
